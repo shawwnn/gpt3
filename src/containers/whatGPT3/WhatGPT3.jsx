4e37ba77ab8e243a0ebdf15df482b691eb091dfc
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-04 23:17:26 +0800

    What is GPT-3 Section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './whatgpt3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="What is GPT-3" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci ea consequatur officia, explicabo velit vero corporis nemo perferendis, eius recusandae exercitationem! Delectus minima facere est."/>
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="Chatbots" text="Adipisicing elit. Adipisci ea consequatur officia, explicabo velit vero corporis nemo perferendis, eius recusandae exercitationem! Delectus minima facere est."/>
+        <Feature title="Knowledgebase" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci ea consequatur officia, explicabo velit vero corporis nemo perferendis, eius recusandae exercitationem! Delectus minima facere est."/>
+        <Feature title="Education" text="Adipisci ea consequatur officia, explicabo velit vero corporis nemo perferendis, eius recusandae exercitationem! Delectus minima facere est."/>
+      </div>
+    </div>
   )
 }
 
